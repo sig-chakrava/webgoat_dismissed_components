5fe666c938bea50f68ff64e5e40f6f8993884b66
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-22 01:09:50 +0530

    update file src/main/java/org/owasp/webgoat/webwolf/FileServer.java

--- src/main/java/org/owasp/webgoat/webwolf/FileServer.java
+++ src/main/java/org/owasp/webgoat/webwolf/FileServer.java
@@ -103,15 +103,15 @@ public class FileServer {
     File[] files = destinationDir.listFiles(File::isFile);
     if (files != null) {
       for (File file : files) {
-        String size = FileUtils.byteCountToDisplaySize(file.length());
-        String link = String.format("files/%s/%s", username, file.getName());
-        uploadedFiles.add(
-            new UploadedFile(file.getName(), size, link, getCreationTime(timezone, file)));
-      }
+        if (!file.getCanonicalPath().startsWith(destinationDir.getCanonicalPath() + File.separator)) {
+        continue; // Skip files that are outside the intended directory
+        }
+            String size = FileUtils.byteCountToDisplaySize(file.length());
+      String link = String.format("files/%s/%s", username, file.getName());
+    uploadedFiles.add(
+new UploadedFile(file.getName(), size, link, getCreationTime(timezone, file)));
     }
-
-    modelAndView.addObject(
-        "files",
+        }
         uploadedFiles.stream().sorted(comparing(UploadedFile::creationTime).reversed()).toList());
     modelAndView.addObject("webwolf_url", "http://" + server + ":" + port + contextPath);
     return modelAndView;
