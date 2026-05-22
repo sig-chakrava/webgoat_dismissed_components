477211dc2a0d496a4af1a11df781bfc154743d1b
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-23 00:06:23 +0530

    update file src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson4.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson4.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson4.java
@@ -41,11 +41,11 @@ public class SqlInjectionLesson4 implements AssignmentEndpoint {
 
   protected AttackResult injectableQuery(String query) {
     try (Connection connection = dataSource.getConnection()) {
-      try (Statement statement =
-          connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)) {
-        statement.executeUpdate(query);
-        connection.commit();
-        ResultSet results = statement.executeQuery("SELECT phone from employees;");
+      String safeQuery = "SELECT * FROM some_table WHERE some_column = ?";
+          try (PreparedStatement preparedStatement = connection.prepareStatement(safeQuery)) {
+        preparedStatement.setString(1, query);
+        preparedStatement.executeUpdate();
+        }
         StringBuilder output = new StringBuilder();
         // user completes lesson if column phone exists
         if (results.first()) {
