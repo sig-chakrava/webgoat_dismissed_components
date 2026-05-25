8426da04078d97aa37c122c9302eb6a82f192a50
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-25 20:57:11 +0530

    update file src/main/java/org/owasp/webgoat/lessons/vulnerablecomponents/VulnerableComponentsLesson.java

--- src/main/java/org/owasp/webgoat/lessons/vulnerablecomponents/VulnerableComponentsLesson.java
+++ src/main/java/org/owasp/webgoat/lessons/vulnerablecomponents/VulnerableComponentsLesson.java
@@ -24,7 +24,7 @@ public class VulnerableComponentsLesson implements AssignmentEndpoint {
   @PostMapping("/VulnerableComponents/attack1")
   public @ResponseBody AttackResult completed(@RequestParam String payload) {
     XStream xstream = new XStream();
-    xstream.setClassLoader(Contact.class.getClassLoader());
+    xstream.allowTypes(new Class[] {Contact.class});
     xstream.alias("contact", ContactImpl.class);
     xstream.ignoreUnknownElements();
     Contact contact = null;
