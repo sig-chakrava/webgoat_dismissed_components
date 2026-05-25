107ea570694795322eb91334a56e6fc920c6b570
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-25 23:23:31 +0530

    update file src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
@@ -52,15 +52,15 @@ public class SqlInjectionChallenge implements AssignmentEndpoint {
 
       try (Connection connection = dataSource.getConnection()) {
         String checkUserQuery =
-            "select userid from sql_challenge_users where userid = '" + username + "'";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(checkUserQuery);
-
-        if (resultSet.next()) {
-          attackResult = failed(this).feedback("user.exists").feedbackArgs(username).build();
-        } else {
-          PreparedStatement preparedStatement =
-              connection.prepareStatement("INSERT INTO sql_challenge_users VALUES (?, ?, ?)");
+            "select userid from sql_challenge_users where userid = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(checkUserQuery);
+        preparedStatement.setString(1, username);
+ResultSet resultSet = preparedStatement.executeQuery();
+        
+          if (resultSet.next()) {
+        attackResult = failed(this).feedback("user.exists").feedbackArgs(username).build();
+          } else {
+              PreparedStatement insertStatement =
           preparedStatement.setString(1, username);
           preparedStatement.setString(2, email);
           preparedStatement.setString(3, password);
