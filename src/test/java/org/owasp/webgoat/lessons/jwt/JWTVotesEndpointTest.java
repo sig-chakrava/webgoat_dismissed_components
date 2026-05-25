9b8aec6400943e3ce9ed91c9a12727b786a0ce68
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-25 17:54:52 +0530

    update file src/test/java/org/owasp/webgoat/lessons/jwt/JWTVotesEndpointTest.java

--- src/test/java/org/owasp/webgoat/lessons/jwt/JWTVotesEndpointTest.java
+++ src/test/java/org/owasp/webgoat/lessons/jwt/JWTVotesEndpointTest.java
@@ -177,24 +177,24 @@ public class JWTVotesEndpointTest extends LessonTest {
             .andExpect(status().isOk())
             .andReturn();
     nodes = new ObjectMapper().readValue(result.getResponse().getContentAsString(), Object[].class);
-    int numberOfVotes = (int) findNodeByTitle(nodes, "Admin lost password").get("numberOfVotes");
-    assertThat(numberOfVotes).isEqualTo(currentNumberOfVotes + 1);
-  }
-
-  private Map<String, Object> findNodeByTitle(Object[] nodes, String title) {
-    for (Object n : nodes) {
-      Map<String, Object> node = (Map<String, Object>) n;
-      if (node.get("title").equals(title)) {
-        return node;
+    Map<String, Object> node = findNodeByTitle(nodes, "Admin lost password");
+    if (node != null) {
+  int numberOfVotes = (int) node.get("numberOfVotes");
+assertThat(numberOfVotes).isEqualTo(currentNumberOfVotes + 1);
+  } else {
+    fail("Node with title 'Admin lost password' not found.");
       }
-    }
-    return null;
+      }
+        
+      private Map<String, Object> findNodeByTitle(Object[] nodes, String title) {
+    for (Object n : nodes) {
+    Map<String, Object> node = (Map<String, Object>) n;
+  if (node.get("title").equals(title)) {
+return node;
   }
-
-  @Test
-  public void guestShouldNotBeAbleToVote() throws Exception {
-    mockMvc
-        .perform(
+  }
+    return null;
+        }
             MockMvcRequestBuilders.post("/JWT/votings/Admin lost password")
                 .cookie(new Cookie("access_token", "")))
         .andExpect(status().isUnauthorized());
