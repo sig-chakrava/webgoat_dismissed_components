107985a1197e304ef6b33e8261eb9887870f1446
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-26 21:11:34 +0530

    update file src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson5a.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson5a.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson5a.java
@@ -38,18 +38,18 @@ public class SqlInjectionLesson5a implements AssignmentEndpoint {
   @ResponseBody
   public AttackResult completed(
       @RequestParam String account, @RequestParam String operator, @RequestParam String injection) {
-    return injectableQuery(account + " " + operator + " " + injection);
+    return injectableQuery(account, operator, injection);
   }
 
-  protected AttackResult injectableQuery(String accountName) {
-    String query = "";
-    try (Connection connection = dataSource.getConnection()) {
-      query =
-          "SELECT * FROM user_data WHERE first_name = 'John' and last_name = '" + accountName + "'";
-      try (Statement statement =
-          connection.createStatement(
-              ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
-        ResultSet results = statement.executeQuery(query);
+  protected AttackResult injectableQuery(String account, String operator, String injection) {
+    String query = "SELECT * FROM accounts WHERE account_name = ? AND operator = ? AND injection = ?";
+    try (Connection connection = dataSource.getConnection();
+      PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+          preparedStatement.setString(1, account);
+      preparedStatement.setString(2, operator);
+          preparedStatement.setString(3, injection);
+              ResultSet results = preparedStatement.executeQuery();
+        // Process results and return the response
 
         if ((results != null) && (results.first())) {
           ResultSetMetaData resultsMetaData = results.getMetaData();
