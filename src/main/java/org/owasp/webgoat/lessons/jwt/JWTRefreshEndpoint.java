f4445d24e82d4891ffbd1b85041b4c2bf6507888
Chakravarthy S M <59598633+sig-chakrava@users.noreply.github.com>
2026-05-29 17:12:48 +0530

    update file src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java

--- src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
+++ src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
@@ -97,7 +97,7 @@ public class JWTRefreshEndpoint implements AssignmentEndpoint {
         }
         return ok(success(this).build());
       }
-      return ok(failed(this).feedback("jwt-refresh-not-tom").feedbackArgs(user).build());
+      return ok(failed(this).feedback("jwt-refresh-not-tom").build());
     } catch (ExpiredJwtException e) {
       return ok(failed(this).output(e.getMessage()).build());
     } catch (JwtException e) {
